7a04f5c2575ad7da58505969fbbc62bef268f61b
sapereaude <atiacht@gmail.com>
2020-01-28 15:00:27 +0300

    comment4

--- git_test_1_1.py
+++ git_test_1_1.py
@@ -1,14 +1,16 @@
 
 def fib(n):
     # 1, 1, 2, 3, 5, 8, 13
+    f_l = [1]
     a, b = 1, 1
     for i in range(n):
         a, b = b, a + b
-        print(b)
-    pass
+        f_l.append(b)
+    return f_l
 
 
 if __name__ == "__main__":
-    fib(10)
+    list_of_fib = fib(10)
+    print(list_of_fib)
 
 
